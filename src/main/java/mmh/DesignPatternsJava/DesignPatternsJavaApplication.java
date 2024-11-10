19ce3c91f67acae102757ab5ad8690c0e9cf9d2a
Mehdi <hoseini@gmail.com>
2024-11-10 17:48:25 +0330

    first commit

---
+++ src/main/java/mmh/DesignPatternsJava/DesignPatternsJavaApplication.java
@@ -0,0 +1,13 @@
+package mmh.DesignPatternsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mmh/DesignPatternsJava/DesignPatternsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package mmh.DesignPatternsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
